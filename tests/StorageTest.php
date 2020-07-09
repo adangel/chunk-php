e8798ede41fedbdbeeca894675ec991116787ac2
Andreas Dangel <andreas.dangel@adangel.org>
2020-07-09 22:15:44 +0200

    Add tests for archive support

--- tests/StorageTest.php
+++ tests/StorageTest.php
@@ -27,6 +27,57 @@ class StorageTest extends TestCase {
         $this->assertEquals('Test file with uuid 5edaecac-6ac4-41c9-980d-e1e109c6b105'. PHP_EOL, stream_get_contents($stream));
     }
 
+    public function testFindFileZipArchiveContent() {
+        $file = $this->storage->findFile('joe', '127ceecd-f10b-4d34-88ed-6f0de2133021');
+        $this->assertEquals(217, $file->getSize());
+        $this->assertEquals('application/zip; charset=binary', $file->getType());
+        $this->assertFile($file, 'test.txt', 'text/plain; charset=us-ascii', 51,
+            'test file for 127ceecd-f10b-4d34-88ed-6f0de2133021'. PHP_EOL);
+
+        // '/' -> not found
+        $this->assertFalse($file->hasContent('/'));
+
+        // '/test.txt' -> works like 'test.txt'
+        $this->assertFile($file, '/' . $fileInArchive, 'text/plain; charset=us-ascii', 51,
+            'test file for 127ceecd-f10b-4d34-88ed-6f0de2133021'. PHP_EOL);
+        $this->assertFile($file, '/style.css', 'text/css', 5);
+        $this->assertFile($file, '/script.js', 'application/javascript', 5);
+        $this->assertFile($file, '/index.html', 'text/html', 5);
+        $this->assertFile($file, '/sub/folder/foo.html', 'text/html', 5);
+    }
+
+    private function assertFile($file, $path, $type, $size, $content = NULL) {
+        $this->assertTrue($file->hasContent($path));
+        $this->assertEquals($type, $file->getType($path));
+        $this->assertEquals($size, $file->getSize($path));
+
+        if (isset($content)) {
+            $stream = fopen('php://memory', 'rw');
+            $file->send($stream, $path);
+            rewind($stream);
+            $this->assertEquals($content, stream_get_contents($stream));
+        }
+    }
+
+    public function testFindFileTarArchiveContent() {
+        $file = $this->storage->findFile('joe', '17621e7d-6e8d-449f-aa48-e81728994afc');
+        $this->assertEquals(217, $file->getSize());
+        $this->assertEquals('application/x-tar', $file->getType());
+        $this->assertFile($file, 'test.txt', 'text/plain; charset=us-ascii', 51,
+            'test file for 17621e7d-6e8d-449f-aa48-e81728994afc'. PHP_EOL);
+
+        // '/' -> not found
+        $this->assertFalse($file->hasContent('/'));
+
+        // '/test.txt' -> works like 'test.txt'
+        $this->assertFile($file, '/' . $fileInArchive, 'text/plain; charset=us-ascii', 51,
+            'test file for 17621e7d-6e8d-449f-aa48-e81728994afc'. PHP_EOL);
+        $this->assertFile($file, '/style.css', 'text/css', 5);
+        $this->assertFile($file, '/script.js', 'application/javascript', 5);
+        $this->assertFile($file, '/index.html', 'text/html', 5);
+        $this->assertFile($file, '/sub/folder/foo.html', 'text/html', 5);
+    }
+
     public function testFindFileExistingWithExtension() {
         $file = $this->storage->findFile('joe', '9ef4d6c0-543b-4824-a05a-4b9a44b3fceb');
         $this->assertNotNull($file);
